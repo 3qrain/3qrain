9f710b6ca6f3bdcda81e2bd1bdafae53bbb79068
3qrain <1360575872@qq.com>
2026-06-10 00:31:32 +0800

    chore: 本地开发vite代理携带ip信息

--- apps/admin/vite.config.ts
+++ apps/admin/vite.config.ts
@@ -1,18 +1,27 @@
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
-import path from "node:path";
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import path from 'node:path'
 
 export default defineConfig({
   plugins: [vue()],
   server: {
     proxy: {
-      "/api": "http://localhost:3010",
-      "/public/": "http://localhost:3010",
-    },
+      // "/api": "http://localhost:3010",
+      '/api': {
+        target: 'http://localhost:3010',
+        changeOrigin: true,
+        configure: proxy => {
+          proxy.on('proxyReq', (proxyReq, req) => {
+            proxyReq.setHeader('X-Forwarded-For', req.socket.remoteAddress || '127.0.0.1')
+          })
+        }
+      },
+      '/public/': 'http://localhost:3010'
+    }
   },
   resolve: {
     alias: {
-      "~": path.resolve(__dirname, "src"),
-    },
-  },
-});
+      '~': path.resolve(__dirname, 'src')
+    }
+  }
+})
